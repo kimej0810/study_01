6baa68b0b013179b8ecff3cb7bd70be2bed74cc4
kimej0810 <56124293+kimej0810@users.noreply.github.com>
2021-07-27 17:36:32 +0900

    kej

---
+++ study/src/main/java/com/study/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("template","들어옴");
+		return "index";
+	}
+	@GetMapping("/board")
+	public String board(Model model) {
+		model.addAttribute("template","views/board");
+		return "views/board";
+	}
+
+}
---
+++ study/src/main/java/com/study/sample/ViewConfig.java
@@ -0,0 +1,21 @@
+package com.study.sample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class ViewConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		return resolver;
+	}
+}
